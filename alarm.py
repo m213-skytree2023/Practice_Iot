be8c20ca04b7aa3c649e6d48b525d239f1fcc20a
m213 <>
2023-06-21 17:11:27 +0900

    modified alarm.py by wll

--- alarm.py
+++ alarm.py
@@ -3,12 +3,15 @@ import datetime
 import schedule
 import subprocess
 
-def task():
+def task():   
     subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?led=white,on")
     time.sleep(10)
     subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?led=white,on")
     time.sleep(10)
     subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?led=white,on")
 
 def set_alarm(transcript = "15時25分に起こして。"):
     dt_today = datetime.datetime.today()
@@ -19,7 +22,6 @@ def set_alarm(transcript = "15時25分に起こして。"):
     alarm_time = f"{dt_today.year}-{dt_today.month}-{dt_today.day} {hour_.zfill(2)}:{minute_.zfill(2)}:00"
     time_array = time.strptime(alarm_time, "%Y-%m-%d %H:%M:%S")
     time_stamp = int(time.mktime(time_array))*1000
-    # 
     while (time_stamp - int(round(time.time() * 1000))) > 0:
         schedule.run_pending()
         time.sleep(1)
