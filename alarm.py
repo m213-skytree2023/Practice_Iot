1e6777dfb32a1b79df46f5d40ae01a22a609fb2e
m212ok <135940712+m212ok@users.noreply.github.com>
2023-06-21 16:47:45 +0900

    Add files via upload

--- alarm.py
+++ alarm.py
@@ -1,30 +1,29 @@
-import urllib3
-from urllib3.exceptions import InsecureRequestWarning
 import time
+import datetime
 import schedule
 import subprocess
 
-urllib3.disable_warnings(InsecureRequestWarning)
+def task():
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=on")
+    time.sleep(10)
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?buzzer=on")
+    time.sleep(10)
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?buzzer=on")
 
 def set_alarm(transcript = "15時25分に起こして。"):
-    #transcript = "15時25分に起こして。"
+    dt_today = datetime.datetime.today()
     id_hour = transcript.find("時")
     id_minute = transcript.find("分")
     hour_ = transcript[:id_hour]
     minute_ = transcript[id_hour+1:id_minute]
-    #アラームをならす
-    def task():
-        subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=on")
-        time.sleep(10)
-        subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?buzzer=on")
-        time.sleep(10)
-        subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?buzzer=on")
-    #時間になったらtask()を実行
-    schedule.every().day.at(f"{hour_.zfill(2)}:{minute_.zfill(2)}").do(task)
-    #print(f"{hour_.zfill(2)}:{minute_.zfill(2)}")
-    while True:
+    alarm_time = f"{dt_today.year}-{dt_today.month}-{dt_today.day} {hour_.zfill(2)}:{minute_.zfill(2)}:00"
+    time_array = time.strptime(alarm_time, "%Y-%m-%d %H:%M:%S")
+    time_stamp = int(time.mktime(time_array))*1000
+    # 
+    while (time_stamp - int(round(time.time() * 1000))) > 0:
         schedule.run_pending()
         time.sleep(1)
+    task()
 
 def stop_alarm():
     subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=off")
