ae9c5db747dd01da97df3847603be52d29e50f71
m212 <m212@skytree2023.site>
2023-06-22 11:35:06 +0900

    add --headless

--- alarm.py
+++ alarm.py
@@ -4,14 +4,14 @@ import schedule
 import subprocess
 
 def task():   
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=on")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?led=white,on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.103:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.103:8000/?led=white,on")
     time.sleep(10)
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?buzzer=on")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?led=white,on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.101:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.101:8000/?led=white,on")
     time.sleep(10)
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?buzzer=on")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?led=white,on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.104:8000/?buzzer=on")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.104:8000/?led=white,on")
 
 #transcript = "15時25分に起こして。"
 def set_alarm(hour_, minute_):
@@ -25,9 +25,9 @@ def set_alarm(hour_, minute_):
     task()
 
 def stop_alarm():
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?buzzer=off")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?buzzer=off")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?buzzer=off")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.103:8000/?led=white,off")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.101:8000/?led=white,off")
-    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe -url http://192.168.0.104:8000/?led=white,off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.103:8000/?buzzer=off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.101:8000/?buzzer=off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.104:8000/?buzzer=off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.103:8000/?led=white,off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.101:8000/?led=white,off")
+    subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe --headless -url http://192.168.0.104:8000/?led=white,off")
